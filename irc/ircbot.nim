684c98424ca31a5e987fbc56fd18b4f8bc927ff5
dom96 <dominikpicheta@googlemail.com>
2011-10-30 19:52:59 +0000

    Updated ircbot with fixes regarding message limiting and flood protection, also added some commands.

--- irc/ircbot.nim
+++ irc/ircbot.nim
@@ -1,4 +1,4 @@
-import irc, sockets, json, os
+import irc, sockets, json, os, strutils
 
 
 type
@@ -44,12 +44,25 @@ proc open(port: TPort = TPort(5123)): TState =
   result.ircClient = irc(ircServer, nick = "NimBot", user = "NimBot",
                          joinChans = joinChans)
 
+proc limitCommitMsg(m: string): string =
+  ## Limits the message to 300 chars and adds ellipsis.
+  var m1 = m
+  if NewLines in m1:
+    m1 = m1.splitLines()[0]
+  
+  if m1.len >= 300:
+    m1 = m1[0..300] & "..."
+
+  if NewLines in m: m1.add($m.splitLines().len & " more lines")
+
+  return m1
 
 proc handleWebMessage(state: var TState, line: string) =
   echo("Got message from hub: " & line)
   var json = parseJson(line)
   if json.existsKey("payload"):
-    for commit in items(json["payload"]["commits"]):
+    for i in 0..min(4, json["payload"]["commits"].len-1):
+      var commit = json["payload"]["commits"][i]
       # Create the message
       var message = ""
       message.add(commit["id"].str[0..6] & " ")
@@ -57,7 +70,7 @@ proc handleWebMessage(state: var TState, line: string) =
       message.add("[+" & $commit["added"].len & " ")
       message.add("±" & $commit["modified"].len & " ")
       message.add("-" & $commit["removed"].len & "]: ")
-      message.add(commit["message"].str)
+      message.add(limitCommitMsg(commit["message"].str))
 
       # Send message to #nimrod.
       state.ircClient.privmsg(joinChans[0], message)
@@ -93,7 +106,29 @@ while True:
   if state.ircClient.poll(event):
     case event.typ
     of EvDisconnected:
-      state.ircClient.connect()
+      while not state.ircClient.isConnected:
+        try:
+          state.ircClient.connect()
+        except:
+          echo("Error reconnecting: ", getCurrentExceptionMsg())
+        
+        echo("Waiting 5 seconds...")
+        sleep(5000)
+      echo("Reconnected successfully!")
     of EvMsg:
       echo("< ", event.raw)
+      if event.cmd == MPrivMsg:
+        var msg = event.params[event.params.high]
+        case msg
+        of "!ping": state.ircClient.privmsg(event.origin, "pong")
+        of "!lag":
+          if state.ircClient.getLag != -1.0:
+            var lag = state.ircClient.getLag
+            lag = lag * 1000.0
+            state.ircClient.privmsg(event.origin,
+                                    $int(lag) &
+                                    "ms between me and the server.")
+          else:
+            state.ircClient.privmsg(event.origin, "Unknown.")
+
       # TODO: ... commands
