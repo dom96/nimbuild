edddfa06340dac26e2d12147829c36e9918b0eeb
Dominik Picheta <dominikpicheta@googlemail.com>
2014-05-03 21:56:26 +0100

    Fix last incorrect change.

--- src/builder.nim
+++ src/builder.nim
@@ -417,11 +417,11 @@ proc restoreBranchSpecificBin(dir, bin, branch: string) =
   let branchSpecificBin = dir / (bin & "_" & branch).exe
   if existsFile(branchSpecificBin):
     copyFile(branchSpecificBin, dir / bin.exe)
+    # Make sure that the binary has +x permissions.
+    inclFilePermissions(dir / bin.exe, {fpUserExec, fpGroupExec, fpOthersExec})
   elif existsFile(dir / bin.exe):
     # Delete the current binary to prevent any issues with old binaries.
     removeFile(dir / bin.exe)
-  # Make sure that the binary has +x permissions.
-  inclFilePermissions(dir / bin.exe, {fpUserExec, fpGroupExec, fpOthersExec})
   
 proc backupBranchSpecificBin(dir, bin, branch: string) =
   if existsFile(dir / bin.exe):
@@ -740,8 +740,8 @@ proc parseReply(line: string, expect: string): Bool =
   var jsonDoc = parseJson(line)
   return jsonDoc["reply"].str == expect
 
-proc hubConnect(state: PState, reconnect: bool) {.gcsafe.}
-proc handleConnect(s: PAsyncSocket, state: PState) {.gcsafe.} =
+proc hubConnect(state: PState, reconnect: bool)
+proc handleConnect(s: PAsyncSocket, state: PState) =
   try:
     # Send greeting
     var obj = newJObject()
@@ -778,13 +778,11 @@ proc handleConnect(s: PAsyncSocket, state: PState) {.gcsafe.} =
     sleep(5000)
     try: hubConnect(state, true) except EOS: echo(getCurrentExceptionMsg()) 
 
-proc handleHubMessage(s: PAsyncSocket, state: PState) {.gcsafe.}
+proc handleHubMessage(s: PAsyncSocket, state: PState)
 proc hubConnect(state: PState, reconnect: bool) =
   state.sock = AsyncSocket()
-  state.sock.handleConnect = proc (s: PAsyncSocket) {.gcsafe.} =
-    handleConnect(s, state)
-  state.sock.handleRead = proc (s: PAsyncSocket) {.gcsafe.} = 
-    handleHubMessage(s, state)
+  state.sock.handleConnect = proc (s: PAsyncSocket) = handleConnect(s, state)
+  state.sock.handleRead = proc (s: PAsyncSocket) = handleHubMessage(s, state)
   state.reconnecting = reconnect
   state.sock.connect(state.cfg.hubAddr, TPort(state.cfg.hubPort))
   state.dispatcher.register(state.sock)
